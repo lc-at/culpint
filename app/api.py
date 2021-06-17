738235808e3bb76f71e5bd35ae1288be511c5d64
loncat <me@lcat.dev>
2021-06-18 03:35:16 +0800

    Implemented new CulpintRecongNgAPI

--- app/api.py
+++ app/api.py
@@ -1,30 +1,16 @@
-import contextlib
-import os
-import sys
-
-from flask import Blueprint, abort, jsonify
+from flask import Blueprint, abort, jsonify, current_app
 
 bp = Blueprint('api', __name__, url_prefix='/api')
-
-from . import app  # noqa
-
-sys.path.insert(1, os.path.join(app.root_path, 'recon-ng'))  # noqa
-
-from recon.core import base  # noqa
-
-rng_base = base.Recon()
-with contextlib.redirect_stdout(open(os.devnull, 'w')):
-    rng_base.start(base.Mode.CLI)
 
 
 @bp.route('/recon-ng/list_modules')
 def rng_list_modules():
-    return jsonify(list(rng_base._loaded_modules.keys()))
+    return jsonify(current_app.rng_api.get_module_names())
 
 
 @bp.route('/recon-ng/module/<path:m_name>')
 def rng_module_details(m_name):
-    module = rng_base._loaded_modules.get(m_name)
+    module = current_app.rng_api.get_module_details(m_name)
     if not module:
         abort(404)
     return jsonify(module.meta)
